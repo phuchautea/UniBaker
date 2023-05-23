7a9b7c9403b49f0ce521978dda2303adc7fdb6a1
phuchau.tea <phuchau.tea@gmail.com>
2023-05-23 15:29:02 +0700

    Initial commit

---
+++ src/main/java/com/uni/baker/BakerApplication.java
@@ -0,0 +1,13 @@
+package com.uni.baker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BakerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uni/baker/BakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uni.baker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
