84db817d253741cbeef35f5d93346005ceab90b5
Hoang Phuc Hau <76581605+phuchautea@users.noreply.github.com>
2023-05-24 15:00:31 +0700

    refactor: rename vouchers table

--- src/main/java/com/uni/baker/entity/Voucher.java
+++ src/main/java/com/uni/baker/entity/Voucher.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @Data
 @Entity
-@Table(name = "voucher")
+@Table(name = "vouchers")
 public class Voucher {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
