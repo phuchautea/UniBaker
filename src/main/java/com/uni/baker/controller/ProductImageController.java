94ce3f3f782312978f1d6d0ca0b7561c38f5f552
CheThanh <chethanh3112@gmail.com>
2023-05-24 21:32:35 +0700

    feat: ProductImage(controller,service,repository)

--- src/main/java/com/uni/baker/controller/ProductImageController.java
+++ src/main/java/com/uni/baker/controller/ProductImageController.java
@@ -1,7 +1,69 @@
-//package com.uni.baker.controller;
+package com.uni.baker.controller;
+
+import com.uni.baker.entity.ProductImage;
+import com.uni.baker.service.ProductImageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/product-Image")
+public class ProductImageController {
+        @Autowired
+        private ProductImageService productImageService;
+
+//        @Autowired
+//        private ProductService productService;
+
+        @GetMapping
+        public String showAllBooks(Model model) {
+            List<ProductImage> proImgs = productImageService.getAll();
+            model.addAttribute("proImgs", proImgs);
+            return "admin/productImage/list";
+        }
+
+//        @GetMapping("/add")
+//        public String addBookForm(Model model) {
+//            model.addAttribute("book", new Book());
+//            model.addAttribute("categories", categoryService.getAllCategories());
+//            return "book/add";
+//        }
+
+//        @PostMapping("/add")
+//        public String addBook(@ModelAttribute("book") Book book) {
+//            bookService.addBook(book);
+//            return "redirect:/books";
+//        }
 //
-//import org.springframework.stereotype.Controller;
+        @GetMapping("/delete/{id}")
+        public String deleteBook (@PathVariable("id") Long id) {
+            ProductImage productImage = productImageService.getById(id);
+            productImageService.delete(id);
+            return "redirect:/product-Image";
+        }
 //
-//@Controller
-//public class ProductImageController {
-//}
+//        @GetMapping("/edit/{id}")
+//        public String editBookForm (@PathVariable("id") Long id, Model model) {
+//            Book editBook = bookService.getBookById(id);
+//            Category cateid = bookService.getBookById(id).getCategory();
+//            model.addAttribute("categories",categoryService.getAllCategories());
+//            if (editBook != null) {
+//                model.addAttribute("book", editBook);
+//                return "book/edit";
+//            } else {
+//                return "not-found";
+//            }
+//        }
+//
+//        @PostMapping("/edit")
+//        public String editBook (@ModelAttribute("book") Book book) {
+//            bookService.updateBook(book);
+//            return "redirect:/books";
+//        }
+//
+
+
+}
--- src/main/java/com/uni/baker/repository/IProductImageRepository.java
+++ src/main/java/com/uni/baker/repository/IProductImageRepository.java
@@ -1,8 +1,9 @@
-//package com.uni.baker.repository;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface IProductImageRepository extends JpaRepository {
-//}
+package com.uni.baker.repository;
+
+import com.uni.baker.entity.ProductImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IProductImageRepository extends JpaRepository<ProductImage, Long> {
+}
